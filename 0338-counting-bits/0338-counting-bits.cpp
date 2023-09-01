b432c1ba9fe0e1422964401572904ea01363211c
Utsav Rai <utsavrai2115@gmail.com>
2023-09-01 07:55:10 +0530

    Time: 6 ms (56.68%), Space: 7.8 MB (89.73%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> countBits(int n) {
         vector<int> siu(n+1);
         for(int i = 0 ; i < n+1 ; i++){
-            siu[i] = __builtin_popcount(i);
+            siu[i] = siu[i >> 1] + (i & 1);
         }
         return siu;
     }
