317094ceb7756be368ef25fe5b41cc6e1ca9ed2f
Utsav Rai <utsavrai2115@gmail.com>
2023-09-17 09:17:25 +0530

    Added solution - LeetHub

---
+++ Leaders in an array - GFG/leaders-in-an-array.cpp
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+// C++ program to remove recurring digits from
+// a given number
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+    
+
+class Solution{
+    //Function to find the leaders in the array.
+    public:
+    vector<int> leaders(int arr[], int n){
+        // Code here
+        vector<int> result;
+    int max_right = arr[n - 1];
+    result.push_back(max_right);
+
+    // Traverse the array from right to left
+    for (int i = n - 2; i >= 0; i--) {
+        if (arr[i] >= max_right) {
+            max_right = arr[i];
+            result.push_back(max_right);
+        }
+    }
+
+    reverse(result.begin(), result.end()); // Reverse the result to maintain order
+    return result;
+        
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+   long long t;
+   cin >> t;//testcases
+   while (t--)
+   {
+       long long n;
+       cin >> n;//total size of array
+        
+        int a[n];
+        
+        //inserting elements in the array
+        for(long long i =0;i<n;i++){
+            cin >> a[i];
+        }
+        Solution obj;
+        //calling leaders() function
+        vector<int> v = obj.leaders(a, n);
+        
+        //printing elements of the vector
+        for(auto it = v.begin();it!=v.end();it++){
+            cout << *it << " ";
+        }
+        
+        cout << endl;
+
+   }
+}
+
+// } Driver Code Ends
