a09a6e5a82ff2b870d1ff993ee5a752c576333e7
Utsav Rai <utsavrai2115@gmail.com>
2023-09-25 12:06:01 +0530

    Time: 11 ms (14.39%), Space: 20.7 MB (7.50%) - LeetHub

--- 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.cpp
+++ 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.cpp
@@ -4,7 +4,7 @@ public:
         int c(0);
         for(int  i=0; i < nums.size() -1 ;i++){
             for(int j =i+1 ; j< nums.size(); j++){
-                if(nums[i]+nums[j] < target ){
+                if(nums[i]+nums[j] < target && i < j){
                     c++;
                 }
             }
