8b94b65eba80e76c345083bdf4bf06da26e8f322
Utsav Rai <utsavrai2115@gmail.com>
2023-09-25 11:42:04 +0530

    Time: 3 ms (94.23%), Space: 14.5 MB (8.90%) - LeetHub

---
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int x=0;
+        for(auto i : operations){
+            if(i[1]=='+') x++;
+            else x--;
+            //ut<<i[1];
+        }
+        return x;
+    }
+};
