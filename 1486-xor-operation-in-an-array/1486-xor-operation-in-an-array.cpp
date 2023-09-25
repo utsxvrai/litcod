928cf19cca4f426cbe2817f72db2a29be7444758
Utsav Rai <utsavrai2115@gmail.com>
2023-09-25 21:01:39 +0530

    Time: 3 ms (9.73%), Space: 7 MB (8.45%) - LeetHub

--- 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.cpp
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.cpp
@@ -3,7 +3,7 @@ public:
     int xorOperation(int n, int start) {
         vector<int> arr(n);
         for(int i  = 0 ; i < n ; i++){
-            arr[i] = start +2*i;
+            arr.push_back(start +2*i);
         }
         int x=0;
         for(auto& i : arr) x^=i;
