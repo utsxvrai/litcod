245e50c1e271bef9d62e0d20c57e56fbc75dd19e
Utsav Rai <utsavrai2115@gmail.com>
2023-11-01 14:46:58 +0530

    Added solution - LeetHub

---
+++ Merge Sort - GFG/merge-sort.cpp
@@ -0,0 +1,100 @@
+//{ Driver Code Starts
+#include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    void merge(int arr[], int l, int m, int r)
+    {
+        int n = r - l + 1;
+    vector<int> siu(n);
+
+    int i = l;
+    int j = m + 1;
+    int k = 0;
+
+    while (i <= m && j <= r)
+    {
+        if (arr[i] <= arr[j])
+        {
+            siu[k++] = arr[i];
+            i++;
+        }
+        else
+        {
+            siu[k++] = arr[j];
+            j++;
+        }
+    }
+
+    while (i <= m)
+    {
+        siu[k++] = arr[i];
+        i++;
+    }
+
+    while (j <= r)
+    {
+        siu[k++] = arr[j];
+        j++;
+    }
+
+    k = 0;
+    for (int i = l; i <= r; i++)
+    {
+        arr[i] = siu[k++];
+    }
+}
+    public:
+    void mergeSort(int arr[], int l, int r)
+    {
+        if(l<r){
+            
+        
+        int mid = l + (r-l)/2;
+        mergeSort(arr, l , mid);
+        mergeSort(arr, mid+1 , r);
+        merge(arr, l , mid, r);
+        }
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+
+int main()
+{
+    int n,T,i;
+
+    scanf("%d",&T);
+
+    while(T--){
+    
+    scanf("%d",&n);
+    int arr[n+1];
+    for(i=0;i<n;i++)
+      scanf("%d",&arr[i]);
+
+    Solution ob;
+    ob.mergeSort(arr, 0, n-1);
+    printArray(arr, n);
+    }
+    return 0;
+}
+// } Driver Code Ends
