ce97f0288f94226d1bf1dad4ea5337c535cd5cf8
Utsav Rai <utsavrai2115@gmail.com>
2024-04-07 07:06:00 +0530

    Time: 56 ms (43.46%), Space: 48.9 MB (78.12%) - LeetHub

---
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    var val=init;
+    if(nums.length == 0) return val;
+    
+    for(let i = 0 ; i < nums.length ; i++){
+        val = fn(val,nums[i]);
+        // console.log(val+" ");
+    }
+    return val;
+    
+};
