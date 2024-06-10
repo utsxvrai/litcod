45ed50a1832adacd271df1774daacd0300cecea4
Utsav Rai <utsavrai2115@gmail.com>
2024-06-10 15:48:47 +0530

    Time: 13 ms (19.32%), Space: 43 MB (5.19%) - LeetHub

--- 3162-find-the-number-of-good-pairs-i/3162-find-the-number-of-good-pairs-i.cpp
+++ 3162-find-the-number-of-good-pairs-i/3162-find-the-number-of-good-pairs-i.cpp
@@ -1,13 +1,36 @@
 class Solution {
 public:
     int numberOfPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-        int ans=0;
-        for(int i = 0 ; i < nums1.size() ; i++){
-            for(int j = 0 ; j < nums2.size() ; j++){
-                if(nums1[i] % (nums2[j]*k) == 0) ans++;
+        unordered_map<int, int> map1;
+        unordered_map<int, int> map2;
+
+        for (int x : nums1) {
+            if (x % k == 0) {
+                int f = x / k;
+                map1[f]++;
             }
         }
-        return ans;
+
+        for (int x : nums2) {
+            map2[x]++;
+        }
+
+        int res = 0;
+        for (const auto& [x, y] : map1) {
+            for (int i = 1; i <= sqrt(x); ++i) {
+                if (x % i == 0) {
+                    int cur = x / i;
+                    if (map2.find(i) != map2.end()) {
+                        res += map2[i] * y;
+                    }
+                    if (cur != i && map2.find(cur) != map2.end()) {
+                        res += map2[cur] * y;
+                    }
+                }
+            }
+        }
+
+        return res;
         
     }
 };
