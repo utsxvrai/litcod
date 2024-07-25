38fb0e645c810d1cb2c6a3c81d2b2e4013fcf046
Utsav Rai <utsavrai2115@gmail.com>
2024-07-25 19:10:52 +0530

    Time: 64 ms (98.32%), Space: 69.5 MB (82.89%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        int count[100001] = {0};
+        int maxv = 0;
+        for (int &n : nums){
+            n += 50000;
+            if(maxv < n) maxv = n;
+            ++count[n];
+        } 
+        
+        int loc = 0;
+        for (int i=0; i ^ 100001; ++i) {
+            while(count[i] > 0) {
+                --count[i];
+                nums[loc++] = i - 50000;
+            }            
+        }
+        
+        return nums;
+    
+        
+    }
+};
